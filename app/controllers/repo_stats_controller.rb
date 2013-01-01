94d2d9384223d7ce81c46958b8e8bb8cb4a1f2ed
Eugene Zimichev <undefz@gmail.com>
2013-01-01 19:44:52 +0400

    Working controller for statistics screen

--- app/controllers/repo_stats_controller.rb
+++ app/controllers/repo_stats_controller.rb
@@ -1,4 +1,14 @@
 class RepoStatsController < ApplicationController
   def show
+    repo_name = params[:repo_name]
+    repo = Repo.where(name: repo_name).first
+    if repo.nil?
+      repo = Repo.create(name: repo_name)
+    end
+    logger.info("#{repo.to_json}")
+    @top_add_contribs = Contribution.where(repo_id: repo.id).order('lines_added').reverse_order.includes(:author).limit(10)
+    @top_delete_contribs = Contribution.where(repo_id: repo.id).order('lines_deleted').reverse_order.includes(:author).limit(10)
+    logger.info("#{@top_add_contribs.to_json}")
+    logger.info("#{@top_delete_contribs.to_json}")
   end
 end
--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,3 +1,4 @@
 class Repo < ActiveRecord::Base
+  has_many :contributions
   attr_accessible :last_commit, :name
 end
