bc4e51ba0ab47e2ede824e143b6a058216f01f00
Eugene Zimichev <undefz@gmail.com>
2013-01-17 02:42:37 +0400

    Functional test for controller getting stats from db

--- test/functional/repo_stats_controller_test.rb
+++ test/functional/repo_stats_controller_test.rb
@@ -2,7 +2,14 @@ require 'test_helper'
 
 class RepoStatsControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, repo_id: 1
+    
+    top_adds = assigns(:top_add_contribs)
+    top_deletes = assigns(:top_delete_contribs)
+
+    assert_equal top_adds.map{ |c| c.author_id }, [7, 6, 5, 4, 3, 2, 1]
+    assert_equal top_deletes.map{ |c| c.author_id }, [1, 2, 3, 4, 5, 6, 7]
+    
     assert_response :success
   end
 
