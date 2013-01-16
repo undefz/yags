f4b91092757010a1125ea22eb5d84cc5027baaec
Eugene Zimichev <undefz@gmail.com>
2013-01-17 02:41:43 +0400

    removed stub tests

--- test/functional/updater_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UpdaterControllerTest < ActionController::TestCase
-  test "should get update_stats" do
-    get :update_stats
-    assert_response :success
-  end
-
-end
